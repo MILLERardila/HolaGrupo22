d1e2a2a2381cef4da1504b60096a7af3f2e83dba
Nicolas Fiquitiva Segura <Nico52105@gmail.com>
2023-06-24 17:22:24 -0500

    se ajusta mensaje

--- NicolasFiquitivaSegura.java
+++ NicolasFiquitivaSegura.java
@@ -1,5 +1,5 @@
 public class NicolasFiquitivaSegura {
     public static void Saludo() {
-        java.lang.System.out.println("Nicolas Fiquitiva Segura dice:        Buen día grupo 22");
+        java.lang.System.out.println("Nicolas Fiquitiva Segura dice:Buen día grupo 22");
     }
 }
