1cc321137afdda1f81e460c239f1be95683f349d
smithramirezc <137573577+smithramirezc@users.noreply.github.com>
2023-06-23 19:21:49 -0500

    Subo mi saludo

--- HolaMundo.java
+++ HolaMundo.java
@@ -3,6 +3,7 @@ public class HolaMundo {
 	public static void main(String[] args) {
 
 		NicolasFiquitivaSegura.Saludo();
+		Smith.Saludo();
 	}
 
 }
---
+++ Smith.java
@@ -0,0 +1,5 @@
+public class Smith {
+    public static void Saludo() {
+    java.lang.System.out.println("Smith dice:        Buenos días grupo");
+}
+}
